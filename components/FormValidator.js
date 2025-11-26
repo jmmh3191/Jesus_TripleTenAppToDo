36fcd60083419d021b0a17d6d7ec036970ba5d47
Jesus <monterojesus911@gmail.com>
2025-11-26 12:29:25 -0800

    sprint7-removedunncessaryfiles

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -25,9 +25,9 @@ class FormValidator {
     errorElement.textContent = "";
   }
 
-  _checkInputValidity(inputElement, errorMessage) {
+  _checkInputValidity(inputElement) {
     if (!inputElement.validity.valid) {
-      this._showInputError(inputElement, errorMessage);
+      this._showInputError(inputElement, inputElement.validationMessage);
     } else {
       this._hideInputError(inputElement);
     }
